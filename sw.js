ab922ac808dd7fe63062f4ed37e8624a4699505f
kdivyaprakash22-oss <kdivyaprakash22@gmail.com>
2025-11-09 02:24:57 +0530

    Add files via upload
    Initial Ikigai app

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'ikigai-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => new Response('Offline'))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => client.navigate(client.url));
+  });
+});
