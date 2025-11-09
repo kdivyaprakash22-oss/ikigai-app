0628d12d9445a977221f9d2fc2aaf34899b8c2fe
kdivyaprakash22-oss <kdivyaprakash22@gmail.com>
2025-11-09 13:20:54 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBm1qr9EER9PV8U8L6RqdeEIR7njNK0uxM",
+  authDomain: "ikigai-app-5987b.firebaseapp.com",
+  projectId: "ikigai-app-5987b",
+  storageBucket: "ikigai-app-5987b.firebasestorage.app",
+  messagingSenderId: "8611888107",
+  appId: "1:8611888107:web:09670e70ade6ad63c237ae"
+};
+
+const VAPID_KEY = "BMCFTEd5oD44SL_umS0L0sTGJpOZqk54snN29FkynnAK1hhhxKsrLKeR3f9Jmc5AXBHIyzorF3oWO_V4Q4YPuhw"
